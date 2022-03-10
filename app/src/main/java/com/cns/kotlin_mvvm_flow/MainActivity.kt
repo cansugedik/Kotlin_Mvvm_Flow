02d984db6313363ab7d5905a9392e862ac635272
cansugedik <cgedik@vtek.com.tr>
2022-03-10 15:13:35 +0300

    Kotlin ile flow projesine başlandı

---
+++ app/src/main/java/com/cns/kotlin_mvvm_flow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cns.kotlin_mvvm_flow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
