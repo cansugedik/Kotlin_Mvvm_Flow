2e0488ee5a5dcd0ad65ed3e56e8529c7615f159c
cansugedik <cgedik@vtek.com.tr>
2022-03-13 13:53:04 +0300

    Güncelleme

--- app/src/main/java/com/cns/kotlin_mvvm_flow/MainActivity.kt
+++ app/src/main/java/com/cns/kotlin_mvvm_flow/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.cns.kotlin_mvvm_flow
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
